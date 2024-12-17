e39cd0273f5f9677426f8ad693d1307cd28d6b95
Gordon Lewis <glewis@berkeley.edu>
2024-12-16 16:31:05 -0800

    add idea on how to do it!

--- day-4/day-4-b.rb
+++ day-4/day-4-b.rb
@@ -2,6 +2,17 @@ samp = File.read('day-4-input.txt')
 
 @data = []
 
+
+# TODO: find the A's and then check the diagonal's 
+
+find X M up and left
+find S bottom right( that's a valid)
+
+.M.S.
+..A..
+.M.S
+
+
 samp.split("\n").each do |x|
   @data << x.split('')
 end
